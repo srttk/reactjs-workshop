c3fde164d6cf0d8b2ff2dbf73b2967323aed8422
Sarath <sarath.hacks@gmail.com>
2016-08-07 12:40:37 +0530

    Added additional lifecycle methods

--- experiments/lifecycle/scripts.js
+++ experiments/lifecycle/scripts.js
@@ -1,10 +1,17 @@
-/* https://facebook.github.io/react/docs/component-specs.html */
 /*
+React Component Lifecycle
 
-
-http://busypeoples.github.io/post/react-component-lifecycle/
-
+https://facebook.github.io/react/docs/component-specs.html
+http://busypeoples.github.io/post/react-component-lifecycle
+https://www.kirupa.com/react/component_lifecycle.htm
 */
+console.info('https://facebook.github.io/react/docs/component-specs.html');
+
+
+// App Component
+
+var mountPoint = document.getElementById('app');
+
 var App = React.createClass({
 
   /* getDefaultProps method */
@@ -19,26 +26,61 @@ var App = React.createClass({
   },
   increase:function(){
     console.log('increase: method clicked');
+    this.setState({count:this.state.count+1});
 
   },
+  /* Lifecycle Methods */
   componentWillMount:function(){
 
-    console.log('componentWillUpdate: Component is about to mount!')
+    console.log('componentWillUpdate: Component is about to mount!');
+    this.setState({count:this.state.count+1});
 
   },
   componentDidMount:function(){
 
     console.log('componentDidMount: Component mounted');
+    this.setState({count:this.state.count+1});
+
+  },
+  shouldComponentUpdate:function(nextProps,nextState){
+
+    console.log('shouldComponentUpdate : before props or states updated');
+    console.log('States:',nextState);
+    if(nextState.count < 10){
+
+      return true;
+
+    }
+    ReactDOM.unmountComponentAtNode(mountPoint);
+    return false;
+
+  },
+  componentWillUpdate:function(nextProps,nextStates){
+    console.log('componentWillUpdate:');
+    console.log(nextStates);
+  },
+  componentDidUpdate:function(prevProps,prevState){
+
+    console.log('componentDidUpdate:');
+    console.log(prevState);
+
+  },
+  componentWillUnmount:function(){
+
+    console.log('componentWillUnmount: DEAD!!!!');
 
   },
   render:function(){
     return(
       <div>
       App
+      <p>Open Developer Console</p>
+      <h4>{this.state.count}</h4>
+      <button onClick={this.increase}>Click</button>
       </div>
     )
   }
 
 });
 
-ReactDOM.render(<App/>,document.getElementById('app'));
+ReactDOM.render(<App/>,mountPoint);
